8c5dd4713bca2336b4fbd6821ba1b41a18c48b1d
Andrei Shtabel <ashtabel94@gmail.com>
2025-05-25 01:40:45 +0700

    Add constexpr to enum flags operators

--- include/storm_audio/enum_flags.h
+++ include/storm_audio/enum_flags.h
@@ -17,35 +17,35 @@ struct is_flag<T, true>: std::false_type {
 // Bit operators for flag types
 template <typename T>
     requires type_traits::is_flag<T>::value
-T operator|(T l, T r)
+constexpr T operator|(T l, T r)
 {
     return static_cast<T>(static_cast<std::underlying_type_t<T>>(l) | static_cast<std::underlying_type_t<T>>(r));
 }
 
 template <typename T>
     requires type_traits::is_flag<T>::value
-T operator&(T l, T r)
+constexpr T operator&(T l, T r)
 {
     return static_cast<T>(static_cast<std::underlying_type_t<T>>(l) & static_cast<std::underlying_type_t<T>>(r));
 }
 
 template <typename T>
     requires type_traits::is_flag<T>::value
-T operator^(T l, T r)
+constexpr T operator^(T l, T r)
 {
     return static_cast<T>(static_cast<std::underlying_type_t<T>>(l) ^ static_cast<std::underlying_type_t<T>>(r));
 }
 
 template <typename T>
     requires type_traits::is_flag<T>::value
-T operator~(T l)
+constexpr T operator~(T l)
 {
     return static_cast<T>(~static_cast<std::underlying_type_t<T>>(l));
 }
 
 template <typename T>
     requires type_traits::is_flag<T>::value
-bool is_flag_enabled(T value, T flag)
+constexpr bool is_flag_enabled(T value, T flag)
 {
     return (value & flag) == flag;
 }
